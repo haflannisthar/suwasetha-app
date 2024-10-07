63d681c5bbfbf9ddfac34f31048a17296a283238
haflan <haflan395@gmail.com>
2024-10-07 20:47:35 +0530

    Initial commit without sensitive data

--- src/main/java/com/suwasethaclinic/Sms/TwilioConfig.java
+++ src/main/java/com/suwasethaclinic/Sms/TwilioConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TwilioConfig {
 
-    private final String ACCOUNT_SID = "AC30069be0828eb8944413c0c0a7cbcd43";
-    private final String AUTH_TOKEN = "5e9b16503cde8a64fbfa43323ec6d6c3";
+    private final String ACCOUNT_SID = "";
+    private final String AUTH_TOKEN = "";
 
     public TwilioConfig() {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
